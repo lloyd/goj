a4f72a2e53fc654186ebedb51857c50c4de651ce
Lloyd Hilaiel <lth@fb.com>
2018-10-09 06:26:26 -0500

    goj to compile on non-amd64 architectures

--- parse_generic.go
+++ parse_generic.go
@@ -5,3 +5,29 @@ package goj
 func hasAsm() bool {
 	return false
 }
+
+// non-asm routines that perform direct byte scanning.
+// bytes.IndexFunc is slower and performs needless coallescing of utf8
+// bytes into runes
+
+func scanNonSpecialStringChars(s []byte, offset int) int {
+	x := 0
+	for x = 0; x < len(s)-offset; x++ {
+		r := rune(s[offset+x])
+		if r == '"' || r == '\\' || r < 0x20 {
+			return x
+		}
+	}
+	return x
+}
+
+func scanNumberChars(s []byte, offset int) int {
+	x := 0
+	for x = 0; x < len(s)-offset; x++ {
+		r := rune(s[offset+x])
+		if r < '0' || r > '9' {
+			return x
+		}
+	}
+	return x
+}
